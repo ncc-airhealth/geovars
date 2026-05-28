9815d01b6b69b67691517947759643cd1b1825ed
hushus <hangm0101@gmail.com>
2026-05-28 15:39:02 +0900

    Add geovariable calculation use case script

---
+++ scripts/usecase.py
@@ -0,0 +1,36 @@
+import duckdb
+import geovars as gv
+
+DB_PATH = "/Users/hus/Library/CloudStorage/Dropbox/패밀리룸/data-pipeline-stac/_data/geovariable-database/version=3.0.1/geovariable-database.db"
+POINT_PATH = "/Users/hus/Library/CloudStorage/Dropbox/패밀리룸/data-pipeline-stac/_data/geovariable-reference-point/version=3.0.1/assets/geovariable-reference-point-all.parquet"
+YEARS = [2000, 2005, 2010, 2015, 2020]
+
+def main():
+
+    # prepare
+    input_df = duckdb.read_parquet(POINT_PATH)
+    calculator = gv.Calculator(
+        database=DB_PATH, 
+        memory_limit="8GB", 
+        workers=8,
+    )
+
+    # calculate
+    gv_df = (
+        calculator
+        .set_input(tbl=input_df, pk="pid", x="longitude", y="latitude", crs="EPSG:4326")
+        .cluster(algorithm="hilbert", size=500)
+        .calc(group="coordinate", year=YEARS)
+        # .safe_calc(group="road_distance", year=YEARS)
+        # .safe_calc(group="road_llw")
+        .df(as_wide=True)
+    )
+
+    # verbose
+    print(gv_df)
+
+
+
+
+if __name__ == "__main__":
+    main()
