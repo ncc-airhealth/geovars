02b1a7b205f4010a8685ce361d18e50b8999a9b5
hushus <hangm0101@gmail.com>
2026-05-28 16:37:36 +0900

    Add coast distance geovariable query

---
+++ geovars/_sql/coast_distance.sql
@@ -0,0 +1,34 @@
+/*
+주어진 위치로부터 가장 가까운 해안선까지의 거리(m)를 계산하는 스크립트
+*/
+
+--------------------------------------------------------------------------------
+-- params
+--------------------------------------------------------------------------------
+
+-- years
+CREATE OR REPLACE TEMP TABLE year AS (
+    SELECT year::UINT16 AS gv_year
+    FROM UNNEST([{{ year | join(', ') }}]) AS t(year)
+);
+
+
+--------------------------------------------------------------------------------
+-- main query
+--------------------------------------------------------------------------------
+
+WITH simple_coastline AS (
+    SELECT year, geom, geom.ST_Simplify(10) AS geom_simple
+    FROM coastline
+)
+SELECT 
+    c.id AS id,
+    y.gv_year,
+    'D_Coast' AS gv_name,
+    ST_Distance(c.geom, l.geom_simple).MIN() AS gv_value,
+FROM 
+    chunk c
+CROSS JOIN simple_coastline l
+LEFT JOIN year y ON l.year = y.gv_year
+GROUP BY c.id, y.gv_year
+;
