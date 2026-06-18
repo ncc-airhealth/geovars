cbff441879cd7461c180bffbe0ee4ca6dfcadb67
hushus <hangm0101@gmail.com>
2026-06-18 10:49:57 +0900

    refactor(sql): update landcover query structure and enhance area calculation logic

--- geovars/_sql/landcover.sql
+++ geovars/_sql/landcover.sql
@@ -1,5 +1,5 @@
 /*
-주어진 위치가 속한 시군구의 자동차 등록대수
+주어진 위치 주변 영역의 토지피복 면적과 비율
 */
 
 -- years
@@ -21,8 +21,9 @@ CREATE OR REPLACE TEMP TABLE _code AS (
 );
 
 
--- data filtering
-CREATE OR REPLACE TEMP TABLE _aoi_h3 AS (
+-- main query
+WITH 
+_aoi_h3 AS (
     SELECT 
         c.geom
         .ST_Buffer(5000, quad_segs:=16)
@@ -36,109 +37,73 @@ CREATE OR REPLACE TEMP TABLE _aoi_h3 AS (
         AS h3
     FROM _chunk c
     ORDER BY h3
-);
-
-CREATE OR REPLACE TEMP TABLE _aoi_landcover AS (
-    WITH _aoi_buffer AS (
-        SELECT geom.ST_Union_Agg().ST_Buffer(5000, quad_segs:=16) AS geom
-        FROM _chunk
-    ), _h3_filtered AS (
-        SELECT t.year, t.code, t.geom
-        FROM landcover t
-        INNER JOIN _aoi_h3 h ON h.h3 = t.h3
-        INNER JOIN _aoi_buffer a ON ST_Intersects(t.geom, a.geom)
-    )
-    SELECT t.year, t.code, t.geom FROM _h3_filtered t
-);
-
-CREATE OR REPLACE TEMP TABLE _overlap_area AS (
+),
+_aoi_buffer AS (
+    SELECT geom.ST_Union_Agg().ST_Buffer(5000, quad_segs:=16) AS geom
+    FROM _chunk
+), _aoi_landcover AS (
+    SELECT t.year, t.code, t.geom, t.geom.ST_Area() AS area
+    FROM landcover t
+    INNER JOIN _aoi_h3 h ON h.h3 = t.h3
+    INNER JOIN _aoi_buffer a ON ST_Intersects(t.geom, a.geom)
+    INNER JOIN _year y ON y.gv_year = t.year
+),
+_chunk_buffer AS (
     SELECT 
         c.id, 
-        b.radius, 
-        y.gv_year, 
-        d.code,
-        c.geom
-            .ST_Buffer(b.radius, quad_segs:=16)
-            .ST_Intersection(t.geom)
-            .ST_Area()
-            .SUM()
-            AS area
+        b.radius,
+        c.geom.ST_Buffer(b.radius, quad_segs:=16) AS geom
     FROM _chunk c
     CROSS JOIN _buffer b
+), 
+_intersecting_area AS (
+    SELECT 
+        c.id, 
+        c.radius, 
+        t.year,
+        t.code,
+        CASE
+            WHEN c.geom.ST_Contains(t.geom)  -- geos 활성화로 인한 메모리 과부하 방지
+            THEN t.area
+            ELSE c.geom.ST_Intersection(t.geom).ST_Area()
+        END .SUM() AS area
+    FROM _chunk_buffer c
+    INNER JOIN _aoi_landcover t ON ST_Intersects(t.geom, c.geom)
+    GROUP BY c.id, c.radius, t.year, t.code
+), 
+_result_area_stat AS (
+    SELECT
+        c.id,
+        y.gv_year,
+        c.radius,
+        d.code,
+        IFNULL(a.area, 0) AS a,
+        IFNULL(a.area, 0) / c.geom.ST_Area() AS p
+    FROM _chunk_buffer c
     CROSS JOIN _year y
     CROSS JOIN _code d
-    LEFT JOIN _aoi_landcover t ON
-        y.gv_year = t.year
-        AND d.code = t.code
-        AND ST_Intersects(t.geom, c.geom)
-    GROUP BY c.id, b.radius, y.gv_year, d.code
-    ORDER BY c.id, b.radius, y.gv_year, d.code
-);
-
-WITH result_a AS (
-    SELECT 
-        id, 
+    LEFT JOIN _intersecting_area a
+        ON c.id = a.id 
+        AND c.radius = a.radius 
+        AND y.gv_year = a.year
+        AND d.code = a.code
+), 
+_result AS (
+    SELECT
+        id,
         gv_year,
-        'LS' || code::VARCHAR || '_' || LPAD(radius::VARCHAR, 4, '0') || '_a' AS gv_name, 
-        area AS gv_value
-    FROM _overlap_area
-), result_p AS (
-    SELECT 
-        id, 
-        gv_year,
-        'LS' || code::VARCHAR || '_' || LPAD(radius::VARCHAR, 4, '0') || '_p' AS gv_name, 
-        area / SUM(area) OVER (PARTITION BY id, gv_year) AS gv_value
-    FROM _overlap_area
+        CONCAT_WS(
+            '_',
+            'LS' || code::VARCHAR,
+            LPAD(radius::VARCHAR, 4, '0'),
+            ap
+        ) AS gv_name, 
+        gv_value,
+    FROM (
+        UNPIVOT _result_area_stat
+        ON a, p
+        INTO NAME ap VALUE gv_value
+    )
+    ORDER BY id, gv_year, gv_name
 )
-SELECT id, gv_year, gv_name, IFNULL(gv_value, 0) AS gv_value FROM result_a
-UNION ALL
-SELECT id, gv_year, gv_name, IFNULL(gv_value, 0) AS gv_value FROM result_p
-;
-
-
-
--- SELECT NULL AS id, NULL AS gv_year, NULL AS gv_name, NULL AS gv_value
--- FROM _chunk c;
-
-
-
-
-
--- WITH _area_stat AS (
---     SELECT 
---         c.id, 
---         y.gv_year, 
---         'LS_' || d.code::VARCHAR || '_' || LPAD(b.radius::VARCHAR, 4, '0') AS prefix, 
---         t.geom
---             .ST_Intersection(c.geom.ST_Buffer(b.radius))
---             .ST_Area() 
---             .SUM()
---             AS area
---     FROM _chunk c
---     CROSS JOIN _year y
---     CROSS JOIN _buffer b
---     CROSS JOIN _code d
---     LEFT JOIN _aoi_landcover t
---         ON t.year = y.gv_year 
---         AND t.code = d.code
---         AND t.geom.ST_DWithin(c.geom, b.radius)
---     GROUP BY c.id, y.gv_year, d.code, b.radius
--- ), _area_result AS (
---     SELECT 
---         id, 
---         gv_year, 
---         prefix || '_a' AS gv_name, 
---         IFNULL(area, 0) AS gv_value
---     FROM _area_stat
--- ), _ratio_result AS (
---     SELECT 
---         id, 
---         gv_year, 
---         prefix || '_p' AS gv_name, 
---         IFNULL(area, 0) / SUM(IFNULL(area, 0)) OVER (PARTITION BY id, gv_year) AS gv_value
---     FROM _area_stat
--- )
--- SELECT * FROM _area_result
--- UNION ALL
--- SELECT * FROM _ratio_result
--- ;
+SELECT * FROM _result;
